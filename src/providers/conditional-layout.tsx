ef46698ad370ba1470207b7c7c1d340071f342d6
MartonRuzsik <r.marton5@gmail.com>
2026-08-08 23:48:15 +0200

    changed header and footer name back because of build error

--- src/providers/conditional-layout.tsx
+++ src/providers/conditional-layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Footer from "@/components/layout/footer";
-import Header from "@/components/layout/header";
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
 import { PublicSession } from "@/types/auth";
 import { usePathname } from "next/navigation";
 
